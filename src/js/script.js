311fd14dffb61f549a7219fe84f42df0281e765f
tsuchanbachan <dwjhf31372@gmail.com>
2023-07-01 21:18:33 +0900

    20230701-３章２のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    /* ハンバーガーボタン */
+    $(".js-hamburger").click(function () {
+        if($(".js-hamburger").hasClass('is-active')) {
+            $('.js-hamburger').removeClass("is-active");
+            $(".js-sp-nav").fadeOut(300);
+        } else {
+            $('.js-hamburger').addClass("is-active");
+            $(".js-sp-nav").fadeIn(300);
+        }
+    });
 
 });
