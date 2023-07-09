efd1cff835b0295b9d3d73668d5df9a1bcaf4119
tsuchanbachan <dwjhf31372@gmail.com>
2023-07-09 21:20:14 +0900

    20230709-３章５まで

--- src/js/script.js
+++ src/js/script.js
@@ -12,4 +12,18 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         }
     });
 
+    /* クラス名「.mySwiper」→「.js-works-swiper」へ変更 */
+    var swiper = new Swiper(".js-works-swiper", {
+        pagination: {
+            /* HTMLのクラス名追加に伴って、「.swiper-pagination」から「.js-works-pagination」へ */
+            el: ".js-works-pagination",
+        },
+        clickable: true,
+        loop: true,
+        autoplay: {
+            /* 3000くらいが適当か？ */
+            delay: 3000,
+        },
+    });
+
 });
